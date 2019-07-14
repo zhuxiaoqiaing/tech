0bc0618e59a48cbf2a1801cd8939afaf8d6d6fb1
zxx <1806320505@qq.com>
2019-07-14 20:15:02 +0800

    flink初始化

---
+++ flink/src/main/java/flink/process/TestProcess.java
@@ -0,0 +1,11 @@
+package flink.process;
+
+/**
+ * Author: zhuxiaoxiang
+ * Date: 2019/7/14 20:14
+ */
+public class TestProcess {
+    public static void main(String[] args) {
+        System.out.println("processing...");
+    }
+}
---
+++ flink/src/main/java/flink/source/TestSource.java
@@ -0,0 +1,11 @@
+package flink.source;
+
+/**
+ * Author: zhuxiaoxiang
+ * Date: 2019/7/14 20:12
+ */
+public class TestSource {
+    public static void main(String[] args) {
+        System.out.println("source....");
+    }
+}
